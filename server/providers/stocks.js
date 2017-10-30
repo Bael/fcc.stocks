e771b7f3859c815efb5ad2ae2fcfa6b4d36519c0
Denis <denkern@gmail.com>
2017-10-30 22:19:10 +0700

    chart experiments

--- server/providers/stocks.js
+++ server/providers/stocks.js
@@ -5,13 +5,25 @@ require('dotenv').config();
 
 async function getStocksBySymbol(symbol) {
 
-    let response;
+    let response = [];
     let API_KEY = process.env.APIKEY;
     try {
 
         const baseUrl = `https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${symbol}&interval=1min&apikey=${API_KEY}`;
 
-        response = await request(baseUrl);
+        let raw = await request(baseUrl);
+
+        let jsonBody = JSON.parse(raw.body)
+        let series = jsonBody["Time Series (1min)"];
+        console.log(jsonBody);
+        console.log(series);
+
+        Object.getOwnPropertyNames(series).sort().forEach(function(val, idx, array) {
+            console.log(val + ' -> ' + series[val]);
+            response.push({x:new Date(val).getTime(), y:series[val]["4. close"]});
+          });
+
+        
 
     } catch (e) {
         console.log(e);
